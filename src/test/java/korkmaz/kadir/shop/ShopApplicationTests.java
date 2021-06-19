e117641fc9d2c4fa0c049c1632c8aefe07a0e775
cskadir <kadirkorkmaz96@gmail.com>
2021-06-19 19:08:24 +0300

    Initial commit

---
+++ src/test/java/korkmaz/kadir/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package korkmaz.kadir.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
